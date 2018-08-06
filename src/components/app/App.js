17ae77dd49da7c9cbb4273e6e58921c92b894928
Jordi M. Zambrano <nickschoey@gmail.com>
2018-08-06 10:08:56 +0200

    starting with the form

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { toggleModal } from '../../actions/toggleModal'
 import { connect } from 'react-redux';
 import { getItems } from '../../actions/getItems'
 import './App.css';
@@ -7,6 +7,7 @@ import logo from '../../assets/Juliett.png';
 import Item from '../item/Item';
 import Cart from '../cart/cart';
 import { updateCrypto } from '../../actions/updateCrypto';
+import Modal from '../modal/modal'
 
 class App extends Component {
 
@@ -16,10 +17,10 @@ class App extends Component {
     setInterval(() => this.props.updateCrypto(), 600000);
 
   }
-  
-  getItems = () => this.props.items.map((item, k) => <Item key={item._id} arrayKey = {k} name={item.name} img={item.imageURL} priceCrypto={item.priceCrypto} priceFiat={item.priceFiat}/>)
 
-  showCart = () => this.props.cart.map((item, index) => <Cart key={index} arrayKey={index} name={item.name} priceFiat={item.priceFiat}/>)
+  getItems = () => this.props.items.map((item, k) => <Item key={item._id} arrayKey={k} name={item.name} img={item.imageURL} priceCrypto={item.priceCrypto} priceFiat={item.priceFiat} />)
+
+  showCart = () => this.props.cart.map((item, index) => <Cart key={index} arrayKey={index} name={item.name} priceFiat={item.priceFiat} />)
 
   render () {
 
@@ -40,6 +41,12 @@ class App extends Component {
           <p>YOUR TOTAL IN FIAT: {this.props.cartTotal} €</p>
           <p>YOUR TOTAL IN ETH: {this.props.cartTotalCrypto} Ξ</p>
         </div>
+        <div>
+          <button className="place-order" onClick={this.props.toggleModal}>
+            <span> ORDER </span>
+          </button>
+          <Modal onClick={this.props.toggleModal} status={this.props.modal} />
+        </div>
       </div>
     );
   }
@@ -51,12 +58,14 @@ const mapStateToProps = (state) => ({
   quote: state.items.quote,
   cart: state.cart.cartItems,
   cartTotal: state.cart.price,
-  cartTotalCrypto: state.cart.cryptoPrice
+  cartTotalCrypto: state.cart.cryptoPrice,
+  modal: state.modal
 });
 
 const mapDispatchToProps = (dispatch) => ({
   getItems: () => dispatch(getItems()),
-  updateCrypto: () =>dispatch(updateCrypto())
+  updateCrypto: () => dispatch(updateCrypto()),
+  toggleModal: () => dispatch(toggleModal())
 
 });
 
