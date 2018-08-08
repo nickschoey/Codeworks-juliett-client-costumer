32ec991c631c5a3850c76d6d99a16a9961d6a3cc
Jordi M. Zambrano <nickschoey@gmail.com>
2018-08-08 22:43:00 +0200

    ADD: Transaction finish

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -17,7 +17,7 @@ class Modal extends Component {
       phone: '',
       email: '',
       wallet: '',
-      cameraFlag:false
+      cameraFlag: false
     }
   }
 
@@ -31,7 +31,7 @@ class Modal extends Component {
 
   handleChange = (e) => this.setState({ [e.target.name]: e.target.value })
 
-  handleCamera = () => this.setState({cameraFlag:!this.state.cameraFlag})
+  handleCamera = () => this.setState({ cameraFlag: !this.state.cameraFlag })
 
   handleSubmit = (e) => {
     e.preventDefault();
@@ -45,31 +45,73 @@ class Modal extends Component {
       cryptoPrice: this.props.cryptoPrice,
       price: this.props.price
     })
+    this.setState({
+      name: '',
+      address: '',
+      phone: '',
+      email: '',
+      wallet: '',
+      cameraFlag: false
+    })
   }
 
   render () {
+    const isWaiting = this.props.order.waiting;
+    const paid = this.props.order.paid;
+    const cryptoPrice = this.props.cryptoPrice;
+    let modalContent;
+    if (!isWaiting && !paid) {
+      modalContent =
+        <div className="modal" data-status={this.props.status}>
+          <div className="modal-left">
+            <form onSubmit={this.handleSubmit}>
+              <h3>Delivery details </h3>
+              <label>Name:</label>
+              <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+              <label>Address:</label>
+              <input type="text" name="address" value={this.state.address} onChange={this.handleChange} />
+              <label>Phone:</label>
+              <input type="text" name="phone" value={this.state.phone} onChange={this.handleChange} />
+              <label>Email:</label>
+              <input type="text" name="email" value={this.state.email} onChange={this.handleChange} />
+              <h3>Payment Details</h3>
+              <label>Your public ETH Wallet: <a onClick={this.handleCamera}><img className="cameraImg" src={camera} /></a></label>
+              <input type="text" name="wallet" value={this.state.wallet} onChange={this.handleChange} />
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+          <div className="modal-right">
+            {this.state.cameraFlag ? <CameraQr /> : <img src={qrCode} />}
+          </div>
+        </div>
+    }
+
+    if (isWaiting && !paid) {
+      modalContent =
+        <div className="modal" data-status={this.props.status}>
+          <div className="modal-left">
+          Your order is being processed. You can transfer {cryptoPrice} Ξ to the wallet represented by the qr on the right.
+          </div>
+          <div className="modal-right">
+          <img src={qrCode}/>
+          </div>
+        </div>
+    }
+    if (!isWaiting && paid) {
+      modalContent =
+        <div className="modal" data-status={this.props.status}>
+          <div className="modal-left">
+          We recieved the transfer! Thank you for your patience!
+          </div>
+          <div className="modal-right">
+            Successful transfer and happy people
+          </div>
+        </div>
+    }
+
     return (
-      <div className="modal" data-status={this.props.status}>
-        <div className="modal-left">
-          <form onSubmit={this.handleSubmit}>
-            <h3>Delivery details </h3>
-            <label>Name:</label>
-            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
-            <label>Address:</label>
-            <input type="text" name="address" value={this.state.address} onChange={this.handleChange} />
-            <label>Phone:</label>
-            <input type="text" name="phone" value={this.state.phone} onChange={this.handleChange} />
-            <label>Email:</label>
-            <input type="text" name="email" value={this.state.email} onChange={this.handleChange} />
-            <h3>Payment Details</h3>
-            <label>Your public ETH Wallet: <a onClick = {this.handleCamera}><img className="cameraImg" src={camera}/></a></label>
-            <input type="text" name="wallet" value={this.state.wallet} onChange={this.handleChange} />
-            <input type="submit" value="Submit" />
-          </form>
-        </div>
-        <div className="modal-right">
-          {this.state.cameraFlag ? <CameraQr/> : <img src={qrCode}/>}
-        </div>
+      <div>
+        {modalContent}
       </div>
     );
   }
@@ -79,7 +121,8 @@ const mapStateToProps = (state) => ({
   items: state.cart.cartItems,
   cryptoPrice: state.cart.cryptoPrice,
   price: state.cart.price,
-  qrData: state.qr
+  qrData: state.qr,
+  order: state.order,
 });
 
 const mapDispatchToProps = (dispatch) => ({
